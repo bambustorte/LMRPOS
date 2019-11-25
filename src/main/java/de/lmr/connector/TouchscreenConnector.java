5b173f24747d16af987f74108982481afe13e0a8
Max Hausch <cheriimoya@googlemail.com>
2019-11-25 22:20:14 +0100

    Fix cash drawer not opening

--- src/main/java/de/lmr/connector/TouchscreenConnector.java
+++ src/main/java/de/lmr/connector/TouchscreenConnector.java
@@ -7,6 +7,7 @@ public class TouchscreenConnector implements Runnable {
     static TouchscreenConnector instance = null;
     static public boolean running = true;
 
+    String devicePath;
     FileInputStream in;
     PrinterConnector printer;
 
@@ -17,28 +18,24 @@ public class TouchscreenConnector implements Runnable {
     }
 
     private TouchscreenConnector(String devicePath, PrinterConnector connector){
-        try {
-            in = new FileInputStream(devicePath);
-            printer = connector;
-        } catch (Exception e) {
-            System.err.println("Could not open the touchscreen device!");
-            e.printStackTrace();
-        }
+        this.devicePath = devicePath;
+        this.printer = connector;
     }
 
     @Override
     public void run() {
         while(running){
             try {
+                in = new FileInputStream(devicePath);
                 in.read();
-
                 System.out.println("opening cash drawer");
-
-                //                printer.openCashDrawer((byte)0,(byte)100,(byte)50);
+                printer.openCashDrawer((byte)0,(byte)100,(byte)50);
+                in.close();
+                in = null;
             } catch (IOException e) {
+                System.err.println("Could not open the touchscreen device!");
                 e.printStackTrace();
             }
-
         }
     }
 
